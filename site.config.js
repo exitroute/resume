4f1ecdc107c056cc7fc0075a594e3538fc61a422
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-03-05 12:07:43 +0100

    implemented sssg and ejs template

--- site.config.js
+++ site.config.js
@@ -1,11 +1,13 @@
 const projects = require('./src/data/projects');
+const resume = require('./src/data/resume');
 
 module.exports = {
   site: {
     title: 'NanoGen',
     description: 'Micro Static Site Generator in Node.js',
     // basePath: process.env.NODE_ENV === 'production' ? '/nanogen' : '',
-    projects
+    projects,
+    resume
   }
   // build: {
   //   outputPath: process.env.NODE_ENV === 'production' ? './docs' : './public'
