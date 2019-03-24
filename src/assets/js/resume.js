410e6b41b2bf9407a0e5a709f5bb082c6f94458e
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-03-24 20:39:28 +0100

    comments removed

--- src/assets/js/resume.js
+++ src/assets/js/resume.js
@@ -3,3 +3,7 @@ require('../scss/style.scss');
 
 
 // this code gets sent to the client a get request is made
+// when button is clicked server.js file logs result
+// 1. create simple server file
+// 2. add event listener
+// 3. send get request to the server 
