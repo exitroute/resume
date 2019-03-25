c70d957a0712beacbc5cfccc9b22438d21005cbc
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-03-25 08:43:08 +0100

    adding button handling

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,2 +1,20 @@
 console.log("hello from js")
 require('../scss/style.scss');
+
+
+// this code gets sent to the client a get request is made
+// when button is clicked server.js file logs result
+// 1. create simple server file
+
+
+// add form, input and button -> use button as is
+// querySelector to get the form element 
+
+const button = document.querySelector(".btn");
+button.addEventListener("click", (e) => {
+  console.log("requested");
+});
+
+// addEventListener(submit)
+// handleEvent -> console.log() 
+// send get request to resume-api 
--- src/assets/js/resume.js
+++ src/assets/js/resume.js
@@ -1,9 +1,2 @@
 console.log("hello from resume")
 require('../scss/style.scss');
-
-
-// this code gets sent to the client a get request is made
-// when button is clicked server.js file logs result
-// 1. create simple server file
-// 2. add event listener
-// 3. send get request to the server 
