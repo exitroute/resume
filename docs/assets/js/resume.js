a6e00efaf725db5c40545b40ea7867745903aa26
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-04-01 15:02:52 +0200

    docs folder updated

--- docs/assets/js/resume.js
+++
@@ -1,4 +0,0 @@
-console.log("hello from resume")
-require('../scss/style.scss');
-
-
--- docs/assets/main.bundle.js
+++ docs/assets/main.bundle.js
@@ -95,9 +95,41 @@
 
 "use strict";
 
+// require("dotenv").config({ path: "./.env" });
+
+__webpack_require__(/*! ../scss/style.scss */ "./src/assets/scss/style.scss");
 
 console.log("hello from js");
-__webpack_require__(/*! ../scss/style.scss */ "./src/assets/scss/style.scss");
+
+// this code gets sent to the client a get request is made
+// when button is clicked server.js file logs result
+// 1. create simple server file
+
+// add form, input and button -> use button as is
+// querySelector to get the form element 
+
+var button = document.querySelector(".auth-form");
+
+button.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("token sent");
+  fetch("http://localhost:3000" + "/auth", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + new URLSearchParams(window.location.search).get("token")
+    }
+  }).then(function (res) {
+    return res.json();
+  }).then(function (res) {
+    console.log(res);
+    // document.querySelector(".resume").textContent = res.value
+  });
+});
+
+// addEventListener(submit)
+// handleEvent -> console.log() 
+// send get request to resume-api
 
 /***/ }),
 
