9e8fcbbbcd977edd3e011ed04257d59acf904835
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-04-01 14:54:32 +0200

    file now reads search params from url

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,13 +1,12 @@
 "use strict";
-
-console.log("hello from js")
+// require("dotenv").config({ path: "./.env" });
 require('../scss/style.scss');
 
+console.log("hello from js")
 
 // this code gets sent to the client a get request is made
 // when button is clicked server.js file logs result
 // 1. create simple server file
-
 
 // add form, input and button -> use button as is
 // querySelector to get the form element 
@@ -17,16 +16,18 @@ const button = document.querySelector(".auth-form");
 button.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log("token sent");
-  fetch("https://localhost:3000/auth", {
+  fetch(`${process.env.DOMAIN}/auth`, {
     method: "POST",
-    body: body,
     headers: {
       "Content-Type": "application/json",
-      "Authorization": "Bearer 1888"
+      "Authorization": `Bearer ${
+        new URLSearchParams(window.location.search).get("token")
+      }`
     }
   }).then(res => res.json())
     .then(res => {
-      document.querySelector(".resume").textContent = res.value
+      console.log(res);
+      // document.querySelector(".resume").textContent = res.value
     })
 });
 
