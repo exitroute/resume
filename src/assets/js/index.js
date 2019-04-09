db7cb6d1a2c603a1389edb43a1d77bdfd9d03ee5
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-04-09 16:30:33 +0200

    refactor navs into functions

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -18,7 +18,6 @@ form.addEventListener("submit", (e) => {
     }
   }).then(res => res.json())
     .then(res => {
-      res.payload.about.overview.map(entry => console.log(entry));
       const html = renderResume(res.payload);
       document.querySelector(".resume").innerHTML = html
     })
@@ -45,188 +44,135 @@ function renderResume(resume) {
         </div>
       </section>
 
-        <div class="row justify-content-end">
-          <div class="col-1">
-            <nav class="nav justify-content-end">
-              <a class="nav-link" href="#skills-and-tools">
-                <small>next</small>
-              </a>
-            </nav>
-          </div>
-        </div>
-      
-        <hr>
-        <section id="skills-and-tools">
-      
-        <div class="row justify-content-end">
-          <div class="col-1">
-            <nav class="nav justify-content-end">
-              <a class="nav-link" href="#top">
-                <small>top</small>
-              </a>
-            </nav>
-          </div>
-        </div>
-
+      ${renderNavNext("skills-and-tools")}
+      <hr>
+      <section id="skills-and-tools">
+        ${renderNavTop()}
+        
         <div class="row mt-3 mb-5">
-        <div class="co1-12 col-md-4 text-md-right">
-          <h2>
-            Skills and Tools
-          </h2>
-        </div>
-        <div class="col-12 col-md-8">
-          ${resume.skillsAndTools.description.html}
-          <div class="row">
-            ${resume.skillsAndTools.detail.map((entry) => { return `
-            <div class="col-6 col-md-4 order-1 order-md-1 my-green-bg py-3 border border-white border border-white text-center">
-              <h5 class="font-weight-bold text-white text-center">
-                ${entry.name}
-              </h5>
-              ${ entry.descriptors.length > 0 
-                ? entry.descriptors.map((descriptor) => { return `
+          <div class="co1-12 col-md-4 text-md-right">
+            <h2>
+              Skills and Tools
+            </h2>
+          </div>
+
+          <div class="col-12 col-md-8"> 
+            ${resume.skillsAndTools.description.html}
+            <div class="row">
+              ${resume.skillsAndTools.detail.map((entry) => { return `
+              <div 
+                class="col-6 col-md-4 order-1 order-md-1 my-green-bg py-3  borderborder-white   border border-white text-center">
+                <h5 class="font-weight-bold text-white text-center">
+                  ${entry.name}
+                </h5>
+                ${ entry.descriptors.length > 0 
+                  ? entry.descriptors.map((descriptor) => { return `
                   <span class="d-inline-block bg-white rounded-left rounded-right m-1p-1">
                     ${descriptor}
                   </span>`}).join("") 
-                : `<p class="bg-white rounded-left rounded-right text-left m-1 p-1">
-                    ${entry.description}
-                  </p>`}
-            </div>`}).join("")}
+                  : `<p class="bg-white rounded-left rounded-right text-left m-1 p-1">
+                      ${entry.description}
+                    </p>`}
+              </div>`}).join("")}
+            </div>
           </div>
-        </div>
-      </section>
-      <div class="row justify-content-end">
-      <div class="col-1">
-        <nav class="nav justify-content-end">
-          <a class="nav-link" href="#recent-work">
-            <small>next</small>
-          </a>
-        </nav>
-      </div>
-    </div>
-    <hr>
+        </section>
 
-    <section id="recent-work">
+        ${renderNavNext("recent-work")}    
+        <hr>
 
-      <div class="row justify-content-end">
-        <div class="col-1">
-          <nav class="nav justify-content-end">
-            <a class="nav-link" href="#top">
-              <small>top</small>
-            </a>
-          </nav>
-        </div>
-      </div>
+        <section id="recent-work">
+        ${renderNavTop()}
 
-      <div class="row mt-3 mb-5">
+          <div class="row mt-3 mb-5">
+            <div class="co1-12 col-md-4 text-md-right">
+              <h2>
+                Recent Work
+              </h2>
+              <p class="text-muted">
+                Since July 2016
+              </p>
+            </div>
 
-        <div class="co1-12 col-md-4 text-md-right">
-          <h2>
-            Recent Work
-          </h2>
-          <p class="text-muted">
-            Since July 2016
-          </p>
-        </div>
-
-        <div class="col-12 col-md-8">
-          ${resume.recentWork.description.html}
-            <div class="row">
-              <div class="col">
-                <div class="accordion" id="recentwork-list">
-                  ${resume.recentWork.activities.map((activity, index) => { return `
-                    <div class="card rounded-0">
-                      <div class="card-header rounded-0 my-green-bg" id "${activity.id}">
-                        <h5 class="mb-0">
-                          <button class="btn btn-link text-white" type="button" data-toggle="collapse" data-target="#${activity.id}Collapse" aria-expanded="${index !== 0 ? false : true}"
-                            aria-controls="${activity.id}Collapse">
-                            ${activity.name}
-                          </button>
-                        </h5>
-                      </div>
-                      <div id="${activity.id}Collapse" class="${index === 0 ? 'collapse show' : 'collapse'}" aria-labelledby="${activity.id}"
-                        data-parent="#recentwork-list">
-                        <div class="card-body">
-                          <ul class="list-unstyled">
-                            ${activity.details.map((detail) => { return `
-                              <li>
-                                ${detail.description}
-                              </li>
-                              <ul class="list-unstyled">
+            <div class="col-12 col-md-8">
+              ${resume.recentWork.description.html}
+              <div class="row">
+                <div class="col">
+                  <div class="accordion" id="recentwork-list">
+                    ${resume.recentWork.activities.map((activity, index) => { return `
+                      <div class="card rounded-0">
+                        <div class="card-header rounded-0 my-green-bg" id "${activity.id}">
+                          <h5 class="mb-0">
+                            <button class="btn btn-link text-white" type="button"   data-toggle="collapse" data-target="#${activity.id}Collapse"   aria-expanded="${index !== 0 ? false : true}"
+                              aria-controls="${activity.id}Collapse">
+                              ${activity.name}
+                            </button>
+                          </h5>
+                        </div>
+                        <div id="${activity.id}Collapse" 
+                          class="${index === 0 ? 'collapse show' : 'collapse'}"   aria-labelledby="${activity.id}"
+                          data-parent="#recentwork-list">
+                          <div class="card-body">
+                            <ul class="list-unstyled">
+                              ${activity.details.map((detail) => { return `
                                 <li>
-                                  <i>
-                                    ${detail.date}
-                                  </i>
+                                  ${detail.description}
                                 </li>
-                              </ul>
-                              `}).join("")
-                            }
-                          </ul>
+                                <ul class="list-unstyled">
+                                  <li>
+                                    <i>
+                                      ${detail.date}
+                                    </i>
+                                  </li>
+                                </ul>
+                                `}).join("")
+                              }
+                            </ul>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                    `}).join("")
-                  }
+                      `}).join("")
+                    }
+                  </div>
                 </div>
               </div>
             </div>
-        </div>
-      </div>
-    </section>
+          </div>
+        </section>
 
-    <div class="row justify-content-end">
-      <div class="col-1">
-        <nav class="nav justify-content-end">
-          <a class="nav-link" href="#management-experience">
-            <small>next</small>
-          </a>
-        </nav>
-      </div>
-    </div>
-    
-    <hr>
+        ${renderNavNext("management-experience")}
+        <hr>
 
-    <section id="management-experience">
-    
-    <div class="row justify-content-end">
-      <div class="col-1">
-        <nav class="nav justify-content-end">
-          <a class="nav-link" href="#top">
-            <small>top</small>
-          </a>
-        </nav>
-      </div>
-    </div>
-
-    <div class="row mt-3 mb-5">
-    <div class="co1-12 col-md-4 text-md-right">
-      <h2>
-        Management Experience
-      </h2>
-      <p class="text-muted">
-        From 2005 until 2016
-      </p>
-    </div>
-    <div class="col-12 col-md-8">
-
-      ${resume.managementExperience.description.html}
-
-          <div class="row man-exp-row justify-content-center mb-5">
-            <div class="col-sm-2 col-3">
-              <div class="list-group text-center" id="listManExp" role="tablist">
-                ${resume.managementExperience.chapters.map(
-                  (chapter, index, arr) => { return `
-                  <a class="list-group-item list-group-item-action 
-                      ${ index === 0 ? 'rounded-0 active' 
-                        : index === arr.length - 1 ? 'rounded-0'
-                        : '' }" 
-                      data-toggle="list" href="#${chapter.id}" role="tab">
-                    ${chapter.name}
-                  </a>
-                  `}).join("")
-                } 
+        <section id="management-experience">
+        ${renderNavTop()}
+        
+        <div class="row mt-3 mb-5">
+          <div class="co1-12 col-md-4 text-md-right">
+            <h2>
+              Management Experience
+            </h2>
+            <p class="text-muted">
+              From 2005 until 2016
+            </p>
+          </div>
+          <div class="col-12 col-md-8">
+            ${resume.managementExperience.description.html}
+            <div class="row man-exp-row justify-content-center mb-5">
+              <div class="col-sm-2 col-3">
+                <div class="list-group text-center" id="listManExp" role="tablist">
+                  ${resume.managementExperience.chapters.map(
+                    (chapter, index, arr) => { return `
+                    <a class="list-group-item list-group-item-action 
+                        ${ index === 0 ? 'rounded-0 active' 
+                          : index === arr.length - 1 ? 'rounded-0'
+                          : '' }" 
+                        data-toggle="list" href="#${chapter.id}" role="tab">
+                      ${chapter.name}
+                    </a>
+                    `}).join("")
+                  } 
+                </div>
               </div>
-            </div>
-
             <div class="col-8">
               <div class="tab-content">
                 ${resume.managementExperience.chapters.map(
@@ -249,41 +195,138 @@ function renderResume(resume) {
           </div>
 
           <h3>Tasks and Responsibilities</h3>
-
           <div class="accordion" id="listManTasks">
-            ${resume.managementExperience.tasksAndResponsibilities.map(
-              (task, index, arr) => { return `
-              <div class="card rounded-0">
-                <div class="card-header rounded-0 my-green-bg" id="${task.id}">
-                  <h5 class="mb-0">
-                    <button class="btn btn-link text-white" type="button" data-toggle="collapse" data-target="#${task.id}Collapse" aria-expanded="${index !== 0 ? false : true}"
-                      aria-controls="${task.id}Collapse">
-                      ${task.name}
-                    </button>
-                  </h5>
-                </div>
-                <div id="${task.id}Collapse" class="${index === 0 ? 'collapse show' : 'collapse'}" aria-labelledby="${task.id}"
-                  data-parent="#listManTasks">
-                  <div class="card-body">
-                    <ul class="list-unstyled">
-                      ${task.details.map((detail) => { return `
-                        <li>
-                          ${detail}
-                        </li>
-                        `}).join("")
-                        }
-                    </ul>
-                  </div>
+          ${resume.managementExperience.tasksAndResponsibilities.map(
+            (task, index, arr) => { return `
+            <div class="card rounded-0">
+              <div class="card-header rounded-0 my-green-bg" id="${task.id}">
+                <h5 class="mb-0">
+                  <button class="btn btn-link text-white" type="button"data-toggle="collapse"   data-target="#${task.id}Collapse"aria-expanded="${index !== 0 ? false :   true}"
+                    aria-controls="${task.id}Collapse">
+                    ${task.name}
+                  </button>
+                </h5>
+              </div>
+              <div id="${task.id}Collapse" class="${index === 0 ? 'collapse show' :'collapse'}  " aria-labelledby="${task.id}"
+                data-parent="#listManTasks">
+                <div class="card-body">
+                  <ul class="list-unstyled">
+                    ${task.details.map((detail) => { return `
+                      <li>
+                        ${detail}
+                      </li>
+                      `}).join("")
+                      }
+                  </ul>
                 </div>
               </div>
-              `}).join("")
-            }
+            </div>
+            `}).join("")
+          }
           </div>
-    </div>
-</section>
+        </div>
+      </section>
 
+      ${renderNavNext("training-education")}
+      <hr>
 
-    <!-- end on main content -->
+      <section id="training-education">
+      ${renderNavTop()}
+
+      <div class="row mt-3 mb-5">
+        <div class="co1-12 col-md-4 text-md-right">
+          <h2>
+            Education and Training
+          </h2>
+          <p class="text-muted">
+            The 1990's
+          </p>
+        </div>
+        <div class="col-12 col-md-8">
+          ${resume.educationAndTraining.description.html}
+          <div class="row">
+            <div class="col">
+              <div class="accordion" id="trainingList">
+                ${resume.educationAndTraining.courses.map(
+                  (course, index, arr) => { return `
+                  <div class="card rounded-0">
+                    <div class="card-header rounded-0 my-green-bg my-green-bg" 
+                          id="${course.id}">
+                      <h5 class="mb-0">
+                        <button 
+                          class="btn btn-link text-white" type="button"   data-toggle="collapse" data-target="#${course.id}Collapse"   aria-expanded="${index !== 0 ? false : true}"
+                          aria-controls="${course.id}Collapse">
+                          ${course.label}
+                        </button>
+                      </h5>
+                    </div>
+                    <div id="${course.id}Collapse" 
+                      class="${index === 0 ? 'collapse show' : 'collapse'}"aria-labelledby="$    {course.id}"
+                      data-parent="#trainingList">
+                      <div class="card-body">
+                        <dl class="row">
+                          <dt class="col-4">
+                            ${course.title}
+                          </dt>
+                          <dd class="col-8">
+                            ${course.organisation}
+                              <br>
+                              ${course.location}
+                                <br>
+                                ${course.date}
+                          </dd>
+                        </dl>
+                        <ul class="list-unstyled">
+                          <li>
+                            ${course.outcome}
+                          </li>
+                          <ul class="small muted">
+                            ${course.details.map((detail) => { return `
+                              <li>
+                                ${detail}
+                              </li>
+                              `}).join("")
+                              }
+                          </ul>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  `}).join("")
+                }
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr>
+        </div>
+      </section>
     </section>
   </main>`
+};
+
+function renderNavTop() { 
+  return `
+    <div class="row justify-content-end">
+      <div class="col-1">
+        <nav class="nav justify-content-end">
+          <a class="nav-link" href="#top">
+            <small>top</small>
+          </a>
+        </nav>
+      </div>
+    </div>` 
+};
+
+function renderNavNext(navItem) {
+  return `
+    <div class="row justify-content-end">
+      <div class="col-1">
+        <nav class="nav justify-content-end">
+          <a class="nav-link" href="#${navItem}">
+            <small>next</small>
+          </a>
+        </nav>
+      </div>
+    </div>`
 };
