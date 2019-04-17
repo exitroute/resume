381ba1523db186f0917dc5ce2291154c016cee06
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-04-17 13:17:28 +0200

    removing resume.ejs

--- site.config.js
+++ site.config.js
@@ -1,11 +1,8 @@
-const resume = require('./src/data/resume');
-
 module.exports = {
   site: {
     title: 'Resume',
     description: 'Micro Static Site Generator in Node.js',
-    basePath: process.env.NODE_ENV === 'production' ? '/nanogen' : '',
-    resume
+    basePath: process.env.NODE_ENV === 'production' ? '/nanogen' : ''
   },
   build: {
     outputPath: process.env.NODE_ENV === 'production' ? './docs' : './public'
