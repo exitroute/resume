06e324684683861793876ad1692c453e79ec55fd
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-06-29 10:29:09 +0200

    bundle update

--- docs/assets/main.bundle.js
+++ docs/assets/main.bundle.js
@@ -103,9 +103,15 @@ console.log("hello from js");
 var form = document.querySelector(".auth-form");
 
 form.addEventListener("submit", function (e) {
+
   e.preventDefault();
   console.log("token sent");
+
+  document.querySelector("div.container > form").setAttribute("class", "d-none");
+  document.querySelector("div.container > hr").setAttribute("class", "d-none");
+
   fetch("http://localhost:3000" + "/auth", {
+
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -114,19 +120,17 @@ form.addEventListener("submit", function (e) {
   }).then(function (res) {
     return res.json();
   }).then(function (res) {
-    var html = renderResume(res.payload);
-    document.querySelector(".resume").innerHTML = html;
+    var resumeData = renderResume(res.payload);
+    var contactData = renderFooter(res.payload);
+    document.querySelector("main.container").innerHTML = resumeData;
+    document.querySelector("#contact").innerHTML = contactData;
   });
 });
 
 function renderResume(resume) {
-  return "\n  <main class=\"container\">\n    <section class=\"main-content\">\n      <section class=\"row mt-5\" id=\"about\">\n        <div class=\"co1-12 col-md-4 text-md-right\">\n          <h2>\n            About\n          </h2>\n        </div>\n        <div class=\"col-12 col-md-8\">\n            " + resume.about.description.html + "  \n          <dl class=\"row\">\n            " + resume.about.overview.map(function (entry) {
-    return "\n            <dt class=\"col-4\">" + entry.stage + "</dt>\n            <dd class=\"col-8\">" + entry.name + "<br>" + entry.detail + "</dd>";
-  }).join("") + "\n          </dl>\n        </div>\n      </section>\n\n      " + renderNavNext("skills-and-tools") + "\n      <hr>\n      <section id=\"skills-and-tools\">\n        " + renderNavTop() + "\n        \n        <div class=\"row mt-3 mb-5\">\n          <div class=\"co1-12 col-md-4 text-md-right\">\n            <h2>\n              Skills and Tools\n            </h2>\n          </div>\n\n          <div class=\"col-12 col-md-8\"> \n            " + resume.skillsAndTools.description.html + "\n            <div class=\"row\">\n              " + resume.skillsAndTools.detail.map(function (entry) {
-    return "\n              <div \n                class=\"col-6 col-md-4 order-1 order-md-1 my-green-bg py-3  borderborder-white   border border-white text-center\">\n                <h5 class=\"font-weight-bold text-white text-center\">\n                  " + entry.name + "\n                </h5>\n                " + (entry.descriptors.length > 0 ? entry.descriptors.map(function (descriptor) {
-      return "\n                  <span class=\"d-inline-block bg-white rounded-left rounded-right m-1p-1\">\n                    " + descriptor + "\n                  </span>";
-    }).join("") : "<p class=\"bg-white rounded-left rounded-right text-left m-1 p-1\">\n                      " + entry.description + "\n                    </p>") + "\n              </div>";
-  }).join("") + "\n            </div>\n          </div>\n        </section>\n\n        " + renderNavNext("recent-work") + "    \n        <hr>\n\n        <section id=\"recent-work\">\n        " + renderNavTop() + "\n\n          <div class=\"row mt-3 mb-5\">\n            <div class=\"co1-12 col-md-4 text-md-right\">\n              <h2>\n                Recent Work\n              </h2>\n              <p class=\"text-muted\">\n                Since July 2016\n              </p>\n            </div>\n\n            <div class=\"col-12 col-md-8\">\n              " + resume.recentWork.description.html + "\n              <div class=\"row\">\n                <div class=\"col\">\n                  <div class=\"accordion\" id=\"recentwork-list\">\n                    " + resume.recentWork.activities.map(function (activity, index) {
+  return "\n    <section class=\"main-content\">\n      <section class=\"row mt-5\" id=\"about\">\n        <div class=\"co1-12 col-md-4 text-md-right\">\n          <h2>\n            About\n          </h2>\n        </div>\n        <div class=\"col-12 col-md-8\">\n            " + resume.about.description.html + "  \n          <dl class=\"row\">\n            " + resume.about.overview.map(function (entry) {
+    return "\n            <dt class=\"col-sm-4\">" + entry.stage + "</dt>\n            <dd class=\"col-8\">" + entry.name + "<br>" + entry.detail + "</dd>";
+  }).join("") + "\n          </dl>\n        </div>\n      </section>\n\n      " + renderNavNext("skills-and-tools") + "\n      <hr>\n      <section id=\"skills-and-tools\">\n        " + renderNavTop() + "\n        \n        <div class=\"row mt-3 mb-5\">\n          <div class=\"co1-12 col-md-4 text-md-right\">\n            <h2>\n              Skills and Tools\n            </h2>\n          </div>\n          <div class=\"col-12 col-md-8\">\n            " + renderDefintionTable(resume.skillsAndTools.techStack) + "\n            " + renderDefintionTable(resume.skillsAndTools.ryanStack) + " \n          </div> \n        </div>   \n      </section>\n\n      " + renderNavNext("recent-work") + "    \n      <hr>\n\n      <section id=\"development-experience\">\n      " + renderNavTop() + "\n      \n        <div class=\"row mt-3 mb-5\">\n          <div class=\"co1-12 col-md-4 text-md-right\">\n            <h2>\n              Development Experience\n            </h2>\n          </div>\n          <div class=\"col-12 col-md-8\">\n            " + renderDefintionTable(resume.developmentExperience.slug) + " \n          </div>    \n        </div>    \n      </section>\n\n      " + renderNavNext("recent-work") + "    \n      <hr>        \n\n        <section id=\"recent-work\">\n        " + renderNavTop() + "\n\n          <div class=\"row mt-3 mb-5\">\n            <div class=\"co1-12 col-md-4 text-md-right\">\n              <h2>\n                Recent Work\n              </h2>\n              <p class=\"text-muted\">\n                Since July 2016\n              </p>\n            </div>\n\n            <div class=\"col-12 col-md-8\">\n              " + resume.recentWork.description.html + "\n              <div class=\"row\">\n                <div class=\"col\">\n                  <div class=\"accordion\" id=\"recentwork-list\">\n                    " + resume.recentWork.activities.map(function (activity, index) {
     return "\n                      <div class=\"card rounded-0\">\n                        <div class=\"card-header rounded-0 my-green-bg\" id \"" + activity.id + "\">\n                          <h5 class=\"mb-0\">\n                            <button class=\"btn btn-link text-white\" type=\"button\"   data-toggle=\"collapse\" data-target=\"#" + activity.id + "Collapse\"   aria-expanded=\"" + (index !== 0 ? false : true) + "\"\n                              aria-controls=\"" + activity.id + "Collapse\">\n                              " + activity.name + "\n                            </button>\n                          </h5>\n                        </div>\n                        <div id=\"" + activity.id + "Collapse\" \n                          class=\"" + (index === 0 ? 'collapse show' : 'collapse') + "\"   aria-labelledby=\"" + activity.id + "\"\n                          data-parent=\"#recentwork-list\">\n                          <div class=\"card-body\">\n                            <ul class=\"list-unstyled\">\n                              " + activity.details.map(function (detail) {
       return "\n                                <li>\n                                  " + detail.description + "\n                                </li>\n                                <ul class=\"list-unstyled\">\n                                  <li>\n                                    <i>\n                                      " + detail.date + "\n                                    </i>\n                                  </li>\n                                </ul>\n                                ";
     }).join("") + "\n                            </ul>\n                          </div>\n                        </div>\n                      </div>\n                      ";
@@ -142,8 +146,18 @@ function renderResume(resume) {
     return "\n                  <div class=\"card rounded-0\">\n                    <div class=\"card-header rounded-0 my-green-bg my-green-bg\" \n                          id=\"" + course.id + "\">\n                      <h5 class=\"mb-0\">\n                        <button \n                          class=\"btn btn-link text-white\" type=\"button\"   data-toggle=\"collapse\" data-target=\"#" + course.id + "Collapse\"   aria-expanded=\"" + (index !== 0 ? false : true) + "\"\n                          aria-controls=\"" + course.id + "Collapse\">\n                          " + course.label + "\n                        </button>\n                      </h5>\n                    </div>\n                    <div id=\"" + course.id + "Collapse\" \n                      class=\"" + (index === 0 ? 'collapse show' : 'collapse') + "\"aria-labelledby=\"$    {course.id}\"\n                      data-parent=\"#trainingList\">\n                      <div class=\"card-body\">\n                        <dl class=\"row\">\n                          <dt class=\"col-4\">\n                            " + course.title + "\n                          </dt>\n                          <dd class=\"col-8\">\n                            " + course.organisation + "\n                              <br>\n                              " + course.location + "\n                                <br>\n                                " + course.date + "\n                          </dd>\n                        </dl>\n                        <ul class=\"list-unstyled\">\n                          <li>\n                            " + course.outcome + "\n                          </li>\n                          <ul class=\"small muted\">\n                            " + course.details.map(function (detail) {
       return "\n                              <li>\n                                " + detail + "\n                              </li>\n                              ";
     }).join("") + "\n                          </ul>\n                        </ul>\n                      </div>\n                    </div>\n                  </div>\n                  ";
-  }).join("") + "\n                </div>\n              </div>\n            </div>\n          </div>\n          <hr>\n        </div>\n      </section>\n    </section>\n  </main>";
+  }).join("") + "\n                </div>\n              </div>\n            </div>\n          </div>\n          <hr>\n        </div>\n      </section>\n    </section>";
 };
+
+function renderDefintionTable(item) {
+  return "\n    " + item.description.html + "\n    <dl class=\"row\">\n      " + item.details.map(function (detail) {
+    return "\n      <dt class=\"col-sm-4\">\n        " + detail.term + "\n      </dt>\n      <dd class=\"col-8\">\n        " + detail.definitions.join(", ") + "\n        " + "\n      </dd>\n      ";
+  }).join("") + "\n    </dl>";
+};
+
+function renderFooter(resume) {
+  return "\n    <div class=\"container\">\n      <div class=\"row justify-content-end\">\n        <div class=\"col-1\">\n          <nav class=\"nav justify-content-end\">\n            <a class=\"nav-link\" href=\"#top\">\n              <small>top</small>\n            </a>\n          </nav>\n        </div>\n      </div>\n      <div class=\"row py-md-5 pb-sm-5 pt-sm-3 pt-0 pb-3\">\n        <div class=\"col-10 offset-2 col-md-4 offset-md-0 text-md-right\">\n          <h3>\n            Contact\n          </h3>\n        </div>\n        <div class=\"col-10 offset-2 offset-md-0 col-md-4 col-lg-3\">\n          " + resume.address.html + "\n        </div>\n        <div class=\"col-10 offset-2 offset-md-0 col-md-4\">\n          <p>\n            <a    href=\"https://www.linkedin.com/in/ryanjamesoshea/\">linkedin.com/ryanjamesoshea</a>\n            <br>\n            <a href=\"https://www.exitroute.org\" target=\"_blank\">www.exitroute.org</a>\n            <br>\n            <a href=\"https://github.com/exitroute\">github.com/exitroute</a>\n            <br>\n          </p>\n        </div>\n      </div>\n    </div>";
+}
 
 function renderNavTop() {
   return "\n    <div class=\"row justify-content-end\">\n      <div class=\"col-1\">\n        <nav class=\"nav justify-content-end\">\n          <a class=\"nav-link\" href=\"#top\">\n            <small>top</small>\n          </a>\n        </nav>\n      </div>\n    </div>";
